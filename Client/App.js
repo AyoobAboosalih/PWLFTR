850fda5e2b3b8195e30b1ea111d02f6bf7054f64
Ayoob Aboosalih <ayoobaboosalih@gmail.com>
2022-03-26 12:11:32 +0530

    attempt 1

--- Client/App.js
+++ Client/App.js
@@ -1,12 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Video from 'react-native-video';
+//import video from '0.mp4'
 
 export default function App() {
+  
+
   return (
     <View style={styles.container}>
       <Text>Adjusting this text!!!!!</Text>
       <StatusBar style="auto" />
+      <Video source={{uri: "0"}}   // Can be a URL or a local file.
+       ref={(ref) => {
+         this.player = ref
+       }}                                      // Store reference
+       onBuffer={this.onBuffer}                // Callback when remote video is buffering
+       onError={this.videoError}               // Callback when video cannot be loaded
+       style={styles.backgroundVideo} />
     </View>
   );
 }
