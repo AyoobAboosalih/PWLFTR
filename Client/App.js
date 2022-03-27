14609758b5827fca6d74d5188b1fa7a5a74c474c
Ayoob Aboosalih <ayoobaboosalih@gmail.com>
2022-03-27 12:38:02 +0530

    Basic Video + Server

--- Client/App.js
+++ Client/App.js
@@ -1,23 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Video from 'react-native-video';
+import { View, StyleSheet, Button } from 'react-native';
+import { Video, AVPlaybackStatus } from 'expo-av';
+//import Video from 'react-native-video';
 //import video from '0.mp4'
+
+
 
 export default function App() {
   
-
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState({});
   return (
     <View style={styles.container}>
-      <Text>Adjusting this text!!!!!</Text>
-      <StatusBar style="auto" />
-      <Video source={{uri: "0"}}   // Can be a URL or a local file.
-       ref={(ref) => {
-         this.player = ref
-       }}                                      // Store reference
-       onBuffer={this.onBuffer}                // Callback when remote video is buffering
-       onError={this.videoError}               // Callback when video cannot be loaded
-       style={styles.backgroundVideo} />
+      <Video
+        ref={video}
+        style={{flex: 1}}
+        source={{
+          uri: 'https://www.w3schools.com/html/mov_bbb.mp4',
+        }}
+        useNativeControls
+        resizeMode="contain"
+        isLooping
+        onPlaybackStatusUpdate={status => setStatus(() => status)}
+      />
+      <View style={styles.buttons}>
+        <Button
+          title={status.isPlaying ? 'Pause' : 'Play'}
+          onPress={() =>
+            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
+          }
+        />
+      </View>
     </View>
   );
 }
@@ -25,8 +39,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+  },
+  video: {
+    alignSelf: 'center',
+    width: 320,
+    height: 200,
+  },
+  buttons: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
