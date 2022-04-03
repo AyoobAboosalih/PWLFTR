6c7727174c1eeb8b6a72ce7d7ef25cb707121902
Ayoob Aboosalih <ayoob.2018324@iit.ac.lk>
2022-04-03 21:01:41 +0530

    importing document picker

--- Client/App.js
+++ Client/App.js
@@ -1,35 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View, StyleSheet, Button } from 'react-native';
-import { Video, AVPlaybackStatus } from 'expo-av';
-//import Video from 'react-native-video';
-//import video from '0.mp4'
-import testVideo from './test.mp4'
+import InputVideoScreen from './Views/InputVideoScreen';
 
 
 export default function App() {
   
-  const video = React.useRef(null);
-  const [status, setStatus] = React.useState({});
+
   return (
     <View style={styles.container}>
-      <Video
-        ref={video}
-        style={styles.video}
-        source={testVideo}
-        useNativeControls
-        resizeMode="contain"
-        isLooping
-        onPlaybackStatusUpdate={status => setStatus(() => status)}
-      />
-      <View style={styles.buttons}>
-        <Button
-          title={status.isPlaying ? 'Pause' : 'Play'}
-          onPress={() =>
-            status.isPlaying ? video.current.pauseAsync() : video.current.playAsync()
-          }
-        />
-      </View>
+      <InputVideoScreen/>
     </View>
   );
 }
