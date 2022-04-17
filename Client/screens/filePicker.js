736eee615a5c419b2d561b8fa8f25f0c2e7a4c31
Ayoob Aboosalih <ayoob.2018324@iit.ac.lk>
2022-04-17 16:23:00 +0530

    network request failed

--- Client/screens/filePicker.js
+++ Client/screens/filePicker.js
@@ -12,15 +12,42 @@ import DocumentPicker, {types} from 'react-native-document-picker';
 
 function FilePicker() {
   const [fileResponse, setFileResponse] = useState([]);
+
   const handleDocumentSelection = useCallback(async () => {
     try {
       const response = await DocumentPicker.pick({
         presentationStyle: 'fullScreen',
         type: types.video,
       });
+      console.log(response);
+      const data = new FormData();
+      data.append('name', 'testName'); // you can append anyone.
+      data.append('photo', {
+        uri: response.uri,
+        type: response.type,
+        name: response.name,
+      });
+
+      fetch('http://192.168.1.4:3000/video', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'multipart/form-data',
+        },
+        body: data,
+      })
+        .then(res => {
+          console.log('this is response' + res);
+        })
+        .catch(function (error) {
+          console.log(
+            'There has been a problem with your fetch operation: ' +
+              error.message,
+          );
+          throw error;
+        });
       setFileResponse(response);
-    } catch (err) {
-      console.warn(err);
+    } catch (e) {
+      console.log(e);
     }
   }, []);
 
--- Server/server.py
+++ Server/server.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from distutils.log import debug
+from flask import Flask, request
+from flask_ngrok import run_with_ngrok
 
 app = Flask(__name__)
+# run_with_ngrok(app)
 
 # Memeber API Route
 
-@app.route("/members")
+@app.route("/video", methods=['GET', 'POST'])
 def members():
+    if(request.method == "POST"):
+        bytesOfVideo = request.get_data()
+        with open('video.mp4', 'wb') as out:
+            out.write(bytesOfVideo)
+        return "Video read"
     return{"members": ["Member1","Member2","Member3"]}
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host = '192.168.1.4', port=3000 ,debug=True)
